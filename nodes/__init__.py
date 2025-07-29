d007fe209051791fdfe067fb0ede950fc10e8b86
Uygar <r.uygar@gmail.com>
2025-07-29 07:01:20 +0300

    Add files via upload

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -25,5 +25,17 @@ for k, v in NODE_DISPLAY_NAME_MAPPINGS.items():
        
 #################################################
 
+from .llm.profile import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+for k, v in NODE_CLASS_MAPPINGS.items():
+    
+    ncm[k] = v
+    
+for k, v in NODE_DISPLAY_NAME_MAPPINGS.items():
+    
+    ndm[k] = v
+       
+#################################################
+
 NODE_CLASS_MAPPINGS = ncm
 NODE_DISPLAY_NAME_MAPPINGS = ndm
