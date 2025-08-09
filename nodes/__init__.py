dce568ae0d035e4102891f2da1882f63b96c0d96
Uygar <r.uygar@gmail.com>
2025-08-10 02:10:25 +0300

    Add files via upload

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,3 +1,6 @@
+import os
+import folder_paths
+
 ncm = {}
 ndm = {}
 
@@ -10,44 +13,20 @@ for k, v in NODE_CLASS_MAPPINGS.items():
 for k, v in NODE_DISPLAY_NAME_MAPPINGS.items():
     
     ndm[k] = v
-       
-#################################################
-
-from .llm.io import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-for k, v in NODE_CLASS_MAPPINGS.items():
-    
-    ncm[k] = v
-    
-for k, v in NODE_DISPLAY_NAME_MAPPINGS.items():
-    
-    ndm[k] = v
-       
-#################################################
-
-from .llm.profile import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-for k, v in NODE_CLASS_MAPPINGS.items():
-    
-    ncm[k] = v
-    
-for k, v in NODE_DISPLAY_NAME_MAPPINGS.items():
-    
-    ndm[k] = v
-       
-#################################################
-
-from .llm.gen import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-for k, v in NODE_CLASS_MAPPINGS.items():
-    
-    ncm[k] = v
-    
-for k, v in NODE_DISPLAY_NAME_MAPPINGS.items():
-    
-    ndm[k] = v
-       
-#################################################
 
 NODE_CLASS_MAPPINGS = ncm
 NODE_DISPLAY_NAME_MAPPINGS = ndm
+
+comfyui_input_dir = folder_paths.get_input_directory() 
+os.makedirs(os.path.join(comfyui_input_dir, "artha"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini"), exist_ok=True)  
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "portrait"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "face"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "body"), exist_ok=True) 
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "form"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "cloth"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "makeup"), exist_ok=True) 
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "scene"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "camera"), exist_ok=True)
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "light"), exist_ok=True) 
+os.makedirs(os.path.join(comfyui_input_dir, "artha", "gemini", "style"), exist_ok=True)
